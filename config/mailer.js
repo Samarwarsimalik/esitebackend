6f8d9093d5e22f6c6f20d36ee438da37baef4a91
samar <samarakhlaqwarsi@gmail.com>
2026-02-16 12:13:59 +0530

    Initial commit

--- config/mailer.js
+++ config/mailer.js
@@ -1,20 +1,24 @@
-import nodemailer from "nodemailer";
-
 export const sendMail = async (email, password) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    }
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
-  await transporter.sendMail({
-    from: "Admin <no-reply@company.com>",
-    to: email,
-    subject: "Your Client Account Password",
-    html: `<h3>Login Credentials</h3>
-           <p>Email: ${email}</p>
-           <p>Password: <b>${password}</b></p>`
-  });
+    const info = await transporter.sendMail({
+      from: `Admin <${process.env.EMAIL_USER}>`,
+      to: email,
+      subject: "Your Client Account Password",
+      html: `<h3>Login Credentials</h3>
+             <p>Email: ${email}</p>
+             <p>Password: <b>${password}</b></p>`,
+    });
+
+    console.log("Email sent:", info.messageId);
+  } catch (err) {
+    console.error("Error sending email:", err);
+  }
 };
